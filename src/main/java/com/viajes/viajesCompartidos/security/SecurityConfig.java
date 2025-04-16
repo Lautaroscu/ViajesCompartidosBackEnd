32d3b20c199fc11b49963cb2ebf5b2b8c72932a7
Lautaro <scuffilautaro@gmail.com>
2025-04-16 12:02:02 -0300

    .

--- src/main/java/com/viajes/viajesCompartidos/security/SecurityConfig.java
+++ src/main/java/com/viajes/viajesCompartidos/security/SecurityConfig.java
@@ -79,7 +79,7 @@
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of("http://localhost:5173" , "http://localhost:8080" , "https://viajescompartidos1.netlify.app" ,"https://rideshared.netlify.app" ,"https://www.mercadopago.com.ar")); // Frontend React
+            configuration.setAllowedOrigins(List.of("http://localhost:5173" , "http://localhost:8080"  ,"https://rideshared.netlify.app" ,"https://www.mercadopago.com.ar")); // Frontend React
             configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS" , "PATCH"));
             configuration.setAllowedHeaders(List.of("*"));
             configuration.setAllowCredentials(true); // Permitir cookies
